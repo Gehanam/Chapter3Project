a0df42cdab25910bae03b69a82b8a2b3c4c71359
Gehanam <cstewa62@stumail.jccc.edu>
2025-04-05 15:46:58 -0500

    Add files via upload

---
+++ Project 3.js
@@ -0,0 +1,27 @@
+
+let fullName = "Chris Stewart";
+console.log(fullName);
+
+
+let desiredAnnualSalary = 85000;
+console.log(desiredAnnualSalary);
+
+
+let isVeteran = true; 
+console.log(isVeteran);
+
+
+let friends = ["John Keller", "Lee Hartwig", "Julie Green"];
+console.log(friends);
+
+
+let friendsSalaries = [91000, 101000, 95000];
+console.log(friendsSalaries);
+
+
+let anotherFriend = {
+    firstName: "Alexa",
+    lastName: "Summers",
+    desiredAnnualSalary: 115000
+};
+console.log(anotherFriend);
